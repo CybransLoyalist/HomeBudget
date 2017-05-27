8d8320a2e8426f39381e5797568a44553fc610d7
Krystyna Slusarczyk <slusarczyk.krystyna@gmail.com>
2017-05-27 11:05:10 +0200

    refactoring of generating color picker

---
+++ HomeBudget/App_Code/ChoosableStyle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeBudget.App_Code
+{
+    public enum ChoosableStyle
+    {
+        Moonlight,
+        Terra
+    }
+}
